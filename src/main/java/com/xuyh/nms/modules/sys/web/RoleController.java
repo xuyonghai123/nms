56d5f647f788185c1bf90523e5c8b0de4e180dc2
xuyonghai123 <120961361@qq.com>
2018-02-25 21:14:07 +0800

    登录查询

--- src/main/java/com/xuyh/nms/modules/sys/web/RoleController.java
+++ src/main/java/com/xuyh/nms/modules/sys/web/RoleController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RoleController {
     private RoleMapper roleMapper;
-
     public Role getRole(){
         return new Role();
     }
