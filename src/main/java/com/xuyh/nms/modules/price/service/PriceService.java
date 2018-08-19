c6f47f01473a5b738ba5979c3ee7dac03ddf06dd
xuyonghai123 <120961361@qq.com>
2018-08-19 17:59:40 +0800

    添加注释

--- src/main/java/com/xuyh/nms/modules/price/service/PriceService.java
+++ src/main/java/com/xuyh/nms/modules/price/service/PriceService.java
@@ -1,4 +1,15 @@
 package com.xuyh.nms.modules.price.service;
 
 public class PriceService {
+    // 1.解析入参。
+
+    // 2.入参非空检验。
+
+    // 3.平台账号有效性校验。
+
+    // 4.判断商品是否在选品池中。
+
+    // 5,判断商品价格查询场景。
+
+    // 6,
 }
